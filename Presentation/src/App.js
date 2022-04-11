1410967eecde580fc601bcd888be47444f964612
karolis.nakutavicius1@gmail.com <karolis.nakutavicius1@gmail.com>
2022-04-11 20:05:08 +0300

    login/register screen

--- Presentation/src/App.js
+++ Presentation/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 export default function App() {
   return (
     <>
-      <div id="login-box">
-        <input type="text" placeholder="Email"></input>
-        <input placeholder="Password" type="password"></input>
-        <button type="button" class="btn btn-primary">Login</button>
-        <button class="btn btn-secondary">Register</button>
+      <div className='flexbox-container-center'>
+        <div style={{textAlign: 'center'}} className="center-children-margin">
+          <h4> Please login or register to continue </h4>
+          <input type="text" placeholder="Email" className="center-horizontal" style={{marginTop: '30px'}}></input>
+          <input placeholder="Password" type="password" className="center-horizontal"></input>
+          <label style={{color: 'red', display: 'none'}}>Erorr</label>
+          <div>
+            <button type="button" className="btn btn-primary" style={{marginRight: '20px'}}>Login</button>
+            <button className="btn btn-secondary">Register</button>
+          </div>
+        </div>
       </div>
     </>
   )
