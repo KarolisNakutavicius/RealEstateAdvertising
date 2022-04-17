50c81134cb6f720a39cc4e6e65a6cd5b7f0c7db0
karolis.nakutavicius1@gmail.com <karolis.nakutavicius1@gmail.com>
2022-04-17 07:37:33 +0300

    Layout cleanup

--- Presentation/src/index.js
+++ Presentation/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Layout from './components/Layout';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { render } from "react-dom";
 import Login from "./components/Login";
@@ -12,7 +11,7 @@ const rootElement = document.getElementById("root");
 render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path={"/home"} element={<Home />} />
         <Route path="/login" element={<Login />} />
