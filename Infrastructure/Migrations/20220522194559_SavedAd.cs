503965528666ddca04a4c859637bb96ed1c24050
karolis.nakutavicius1@gmail.com <karolis.nakutavicius1@gmail.com>
2022-05-29 19:31:02 +0300

    migration

---
+++ Infrastructure/Migrations/20220522194559_SavedAd.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class SavedAd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SavedAdvertisements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    AdvertisementId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SavedAdvertisements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SavedAdvertisements_Advertisements_AdvertisementId",
+                        column: x => x.AdvertisementId,
+                        principalTable: "Advertisements",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SavedAdvertisements_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SavedAdvertisements_AdvertisementId",
+                table: "SavedAdvertisements",
+                column: "AdvertisementId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SavedAdvertisements_UserId",
+                table: "SavedAdvertisements",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SavedAdvertisements");
+        }
+    }
+}
